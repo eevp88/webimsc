4b3ac57cee6862a7e52debf2269a83bde6da9443
Enzo Vera <eevp88@gmail.com>
2024-05-16 00:07:07 -0400

    sincronizando master con main

--- src/pages/api/sendEmail.json.ts
+++ src/pages/api/sendEmail.json.ts
@@ -1,81 +1,56 @@
-import type { APIRoute } from "astro"
-import { string } from "astro/zod";
-import { Resend } from 'resend';
+import type { APIRoute } from "astro";
+import { Resend } from "resend";
 
 interface JsonRequest {
-    // Tus otras propiedades
-   name:string;
-   email:string;
-   message:string;
-   disclaimer:string;
+  // Tus otras propiedades
+  name: string;
+  email: string;
+  message: string;
+  disclaimer: string;
+}
+
+export const POST: APIRoute = async ({ locals, request }) => {
+  const { API_RESEND, TO_RESEND, FROM_RESEND } = locals.runtime.env;
+
+  const data: JsonRequest = await request.json();
+  let { name, email, message, disclaimer } = data;
+  let subject = `Mensaje de contacto ${name} `;
+
+  let html = JSON.stringify(data);
+  let text = JSON.stringify(data);
+
+  if (!name || !email || !message) {
+    return new Response(null, {
+      status: 404,
+      statusText: "No proporcionó los datos correctos.",
+    });
   }
 
+  let from = FROM_RESEND;
+  let to = TO_RESEND;
+  const resend: Resend = new Resend(API_RESEND);
 
-export const GET: APIRoute = ( context) => {
-    console.log(context.locals.runtime)
-    return new Response(JSON.stringify({
-        message: "¡Esto es un GET!"
-      })
-    )
-  }
-  
-  export const POST: APIRoute = async ( { locals , request }) => {
-    const {API_RESEND, TO_RESEND,FROM_RESEND} = locals.runtime.env
-   
-    const data : JsonRequest = await request.json()
-    let {name , email, message, disclaimer } = data
-    let subject= `Mensaje de contacto ${name} `;
-    
-    let html = JSON.stringify(data)
-    let text = JSON.stringify(data)
-    
+  const send = await resend.emails.send({ from, to, subject, html, text });
 
-    if ( !name || !email || !message) {
-        return new Response(null, {
-          status: 404,
-          statusText: "No proporcionó los datos correctos.",
-        });
-    }
-
-    let from =  FROM_RESEND
-    let to = TO_RESEND 
-    const resend: Resend = new Resend(API_RESEND);
-    
-    const send = await resend.emails.send({ from , to, subject , html , text});
-
-    if (send.data) {
-        return new Response(
-          JSON.stringify({
-            message: send.data,
-          }),
-          {
-            status: 200,
-            statusText: "success",
-          }
-        );
-      } else {
-        return new Response(
-          JSON.stringify({
-            message: send.error,
-          }),
-          {
-            status: 500,
-            statusText: "Internal Server Error",
-          }
-        );
+  if (send.data) {
+    return new Response(
+      JSON.stringify({
+        message: send.data,
+      }),
+      {
+        status: 200,
+        statusText: "success",
       }
+    );
+  } else {
+    return new Response(
+      JSON.stringify({
+        message: send.error,
+      }),
+      {
+        status: 500,
+        statusText: "Internal Server Error",
+      }
+    );
   }
-  
-  export const DELETE: APIRoute = ({ request }) => {
-    return new Response(JSON.stringify({
-        message: "¡Esto es un DELETE!"
-      })
-    )
-  }
-  
-  export const ALL: APIRoute = ({ request }) => {
-    return new Response(JSON.stringify({
-        message: `¡Esto fue un ${request.method}!`
-      })
-    )
-  }
+};
