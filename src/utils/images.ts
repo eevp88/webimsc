8e230557ce2c6dbc685cba41a0f7986329dd6c0b
Enzo Vera <enzo.vera@west-ingenieria.cl>
2024-08-16 12:25:22 -0400

    correccion

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -35,12 +35,12 @@ export const findImage = async (
   }
 
   // Relative paths or not "~/assets/"
-  if (!imagePath.startsWith('~/assets/images')) {
+  if (!imagePath.startsWith('@/assets/images')) {
     return imagePath;
   }
 
   const images = await fetchLocalImages();
-  const key = imagePath.replace('~/', '/src/');
+  const key = imagePath.replace('@/', '/src/');
 
   return images && typeof images[key] === 'function'
     ? ((await images[key]()) as { default: ImageMetadata })['default']
