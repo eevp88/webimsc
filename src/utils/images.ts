fd7a2ff08d686db09336e3cd5d85127aae4123d7
Enzo Vera <enzo.vera@west-ingenieria.cl>
2024-08-20 15:40:30 -0400

    avance

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -2,7 +2,7 @@ import { getImage } from 'astro:assets';
 import type { ImageMetadata } from 'astro';
 import type { OpenGraph } from '@astrolib/seo';
 
-const load = async function (imageName:string) {
+const load = async function () {
   let images: Record<string, () => Promise<unknown>> | undefined = undefined;
   try {
     images = import.meta.glob('@/assets/images/**/*.{jpeg,jpg,png,tiff,webp,gif,svg,JPEG,JPG,PNG,TIFF,WEBP,GIF,SVG}');
