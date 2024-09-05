6a49584ad5872624327cae8b83e11e96ac3ca110
Enzo Vera <enzo.vera@west-ingenieria.cl>
2024-09-05 17:29:24 -0400

    avance

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			animation: {
+				activate: 'activate 1s linear infinite',  // Animación personalizada
+			},
 			colors: {
 				primary: 'var(--aw-color-primary)',
 				secondary: 'var(--aw-color-secondary)',
@@ -17,7 +20,14 @@ export default {
 			  serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
 			  heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
 			},
-		  },
+      keyframes: {
+        activate: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+
+		  }
+    }
 	},
 	darkMode: 'class',
 	plugins: [],
